8230d3d8a7141d6d7c8c08c1e9565b32b9dd3159
Simran Bardhan <simranbardhan13@gmail.com>
2023-11-10 17:51:15 +0530

    major part done

--- src/app/components/repoview/repoview.component.ts
+++ src/app/components/repoview/repoview.component.ts
@@ -16,13 +16,57 @@ export class RepoviewComponent {
 
   getTagData(path: string) {
     this.apiService.getData(path).subscribe(
-      (data) => {
+      (data: any) => {
         console.log(data, 'tagData');
-        this.tagData = data;
+        this.tagData = data.slice(0, 5);
       },
       (error) => {
         // alert(error);
       }
     );
+  }
+
+  getElapsedTime(updatedAt: string) {
+    var currentDate = new Date();
+
+    return this.timeDifference(currentDate.toString(), updatedAt)
+  }
+
+  timeDifference(current: string, previous: string) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+    
+    var currentDate = Date.parse(current);
+    var previousDate = Date.parse(previous);
+
+    var elapsed = currentDate - previousDate;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
   }
 }
